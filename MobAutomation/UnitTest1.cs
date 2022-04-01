43bc7d9539f4e179774dc6cd047e3c2ab2464751
vishnuprakash9845 <vishnuprakash9845@gmail.com>
2022-04-01 17:17:31 +0530

    second commit

--- MobAutomation/UnitTest1.cs
+++ MobAutomation/UnitTest1.cs
@@ -5,6 +5,7 @@ using OpenQA.Selenium.Appium.Android;
 using System;
 using System.IO;
 using System.Reflection;
+using System.Threading;
 
 namespace MobAutomation
 {
@@ -14,7 +15,7 @@ namespace MobAutomation
         [TestMethod]
         public void InstallAppTest()
         {
-            Console.WriteLine("Testing");
+            Console.WriteLine("Native App Automation");
 
             string path = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             string appPath = Path.GetFullPath(Path.Combine(path, @"..\..\..\..\\MobAutomation\\App\\Demo.apk"));
@@ -26,6 +27,33 @@ namespace MobAutomation
 
             AndroidDriver<IWebElement> driver = new AndroidDriver<IWebElement>(new Uri("http://127.0.0.1:4723/wd/hub"), option);
 
+            Console.WriteLine(driver.PageSource);
+
+            Thread.Sleep(5000);
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void WebAppTest()
+        {
+            Console.WriteLine("Native App Automation");
+
+            AppiumOptions option = new AppiumOptions();
+            option.AddAdditionalCapability("platformName", "android");
+            option.AddAdditionalCapability("deviceName", "mototrola one");//mototrola one
+            option.AddAdditionalCapability("browserName", "Chrome");
+            option.AddAdditionalCapability("chromedriverExecutable", @"C:\Users\muniswamyv\Downloads\Appium\chromedriver_win32\chromedriver.exe");
+
+            AndroidDriver<IWebElement> driver = new AndroidDriver<IWebElement>(new Uri("http://127.0.0.1:4723/wd/hub"), option);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+
+            driver.Url = "https://nasscom.in/about-us/contact-us";
+            Console.WriteLine(driver.PageSource);
+
+            driver.FindElementByXPath("//a[contains(.,'New User')]").Click();
+
+            Thread.Sleep(5000);
+            driver.Quit();
         }
     }
 }
