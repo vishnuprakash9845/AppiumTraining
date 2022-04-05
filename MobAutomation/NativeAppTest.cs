2bc7474b8a9843ed6acfdecac3b1e98d8cf75244
vishnuprakash9845 <vishnuprakash9845@gmail.com>
2022-04-05 14:31:29 +0530

    Day-3 Work

--- MobAutomation/NativeAppTest.cs
+++ MobAutomation/NativeAppTest.cs
@@ -6,6 +6,7 @@ using OpenQA.Selenium.Appium.MultiTouch;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Reflection;
 using System.Text;
@@ -200,23 +201,196 @@ namespace MobAutomation
             driver.FindElementByAndroidUIAutomator("UiSelector().textContains(\"Arts and humanities\")").Click();
 
             //Scroll-1
-            ScrollToElementAndClick(driver, "Art of Asia");
-
-            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(0);
-            //while (driver.FindElementsByAndroidUIAutomator("UiSelector().textContains(\"Art of Asia\")").Count==0)
-            //{
-            //    TouchAction action = new TouchAction(driver);
-            //    action.Press(349, 1015).Wait(1000).MoveTo(349,400).Release().Perform();
-            //}
-
-            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            //driver.FindElementByAndroidUIAutomator("UiSelector().textContains(\"Art of Asia\")").Click();
+            //ScrollToElementAndClick(driver, "Art of Asia");
+            //ScrollToElementByPercentageAndClick(driver, "Art of Asia");
+            ScrollToElementByUISelectorAndClick(driver, "Art of Asia");
 
             //Scroll-2
-            ScrollToElementAndClick(driver, "Himalayas");
+            //ScrollToElementAndClick(driver, "Himalayas");
+            //ScrollToElementByPercentageAndClick(driver, "Himalayas");
+            ScrollToElementByUISelectorAndClick(driver, "Himalayas");
 
             //Scroll-3 : Click on Cabinet for strong offering
-            ScrollToElementAndClick(driver, "Cabinet for storing offerings");
+            //ScrollToElementAndClick(driver, "Cabinet for storing offerings");
+            //ScrollToElementByPercentageAndClick(driver, "Cabinet for storing offerings");
+            ScrollToElementByUISelectorAndClick(driver, "Cabinet for storing offerings");
+
+
+
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void KhanSwipeCommandsTest()
+        {
+            Console.WriteLine("Native App Automation");
+
+            AppiumOptions option = new AppiumOptions();
+            option.AddAdditionalCapability("platformName", "android");
+            option.AddAdditionalCapability("deviceName", "mototrola one");
+            option.AddAdditionalCapability("appActivity", "org.khanacademy.android.ui.library.MainActivity");
+            option.AddAdditionalCapability("appPackage", "org.khanacademy.android");
+            option.AddAdditionalCapability("noReset", true);
+
+            AndroidDriver<IWebElement> driver = new AndroidDriver<IWebElement>(new Uri("http://127.0.0.1:4723/wd/hub"), option);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            //Search
+            driver.FindElementById("org.khanacademy.android:id/tab_bar_button_search").Click();
+
+            //Option Arts and humanities
+            driver.FindElementByAndroidUIAutomator("UiSelector().textContains(\"Arts and humanities\")").Click();
+
+            //Dictionary<string, object> dic = new Dictionary<string, object>();
+            //dic.Add("strategy", "-android uiautomator");
+            //dic.Add("selector", "UiSelector().textContains(\"Asia\")");
+            //driver.ExecuteScript("mobile: scroll", dic);
+
+            //Not working
+            //Dictionary<string, object> dic = new Dictionary<string, object>();
+            //dic.Add("strategy", "xpath");
+            //dic.Add("selector", By.XPath("//*[contains(@text,'Asia')]"));
+            //driver.ExecuteScript("mobile: scroll", dic);
+
+            ScrollToElementByMobileCommandsAndClick(driver,"Asia");
+
+            ScrollToElementByMobileCommandsAndClick(driver, "Himalayas");
+
+            ScrollToElementByMobileCommandsAndClick(driver, "Cabinet for storing offerings");
+
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void listSMSTest()
+        {
+            Console.WriteLine("Native App Automation");
+
+            AppiumOptions option = new AppiumOptions();
+            option.AddAdditionalCapability("platformName", "android");
+            option.AddAdditionalCapability("deviceName", "mototrola one");
+            option.AddAdditionalCapability("appActivity", "org.khanacademy.android.ui.library.MainActivity");
+            option.AddAdditionalCapability("appPackage", "org.khanacademy.android");
+            option.AddAdditionalCapability("noReset", true);
+            option.AddAdditionalCapability("unlockType", "password");
+            option.AddAdditionalCapability("unlockKey", "Vishnu@123");
+
+
+            AndroidDriver<IWebElement> driver = new AndroidDriver<IWebElement>(new Uri("http://127.0.0.1:4723/wd/hub"), option);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            Dictionary<string, object> dic = new Dictionary<string, object>();
+            dic.Add("max", 2);
+            object text = driver.ExecuteScript("mobile: listSms", dic);
+            Console.WriteLine(text);
+            string sms = Convert.ToString(text);
+            Console.WriteLine(sms);
+            
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void MobileShellCommandsTest()
+        {
+            Console.WriteLine("Native App Automation");
+
+            AppiumOptions option = new AppiumOptions();
+            option.AddAdditionalCapability("platformName", "android");
+            option.AddAdditionalCapability("deviceName", "mototrola one");
+            option.AddAdditionalCapability("appActivity", "org.khanacademy.android.ui.library.MainActivity");
+            option.AddAdditionalCapability("appPackage", "org.khanacademy.android");
+            option.AddAdditionalCapability("noReset", true);
+            option.AddAdditionalCapability("unlockType", "password");
+            option.AddAdditionalCapability("unlockKey", "Vishnu@123");
+
+            AndroidDriver<IWebElement> driver = new AndroidDriver<IWebElement>(new Uri("http://127.0.0.1:4723/wd/hub"), option);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            //Search
+            driver.FindElementById("org.khanacademy.android:id/tab_bar_button_search").Click();
+
+            //Option Arts and humanities
+            driver.FindElementByAndroidUIAutomator("UiSelector().textContains(\"Arts and humanities\")").Click();
+
+            Dictionary<string, object> dic = new Dictionary<string, object>();
+            dic.Add("command", "input touchscreen swipe 349 1015 349 400");
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(0);
+            while (driver.FindElementsByAndroidUIAutomator($"UiSelector().textContains(\"Art of Asia\")").Count == 0)
+            {
+                driver.ExecuteScript("mobile: shell",dic);
+            }
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            driver.FindElementByAndroidUIAutomator($"UiSelector().textContains(\"Art of Asia\")").Click();
+
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void CarsAppTest()
+        {
+            Console.WriteLine("Native App Automation");
+
+            AppiumOptions option = new AppiumOptions();
+            option.AddAdditionalCapability("platformName", "android");
+            option.AddAdditionalCapability("deviceName", "mototrola one");
+            option.AddAdditionalCapability("appActivity", "com.cuvora.carinfo.splash.SplashScreenActivity");
+            option.AddAdditionalCapability("appPackage", "com.cuvora.carinfo");
+            option.AddAdditionalCapability("noReset", true);
+            option.AddAdditionalCapability("unlockType", "password");
+            option.AddAdditionalCapability("unlockKey", "Vishnu@123");
+
+            AndroidDriver<IWebElement> driver = new AndroidDriver<IWebElement>(new Uri("http://127.0.0.1:4723/wd/hub"), option);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            IReadOnlyCollection<IWebElement> options = driver.FindElementsByXPath("//android.widget.LinearLayout[@resource-id='com.cuvora.carinfo:id/linearLayout']/android.widget.ImageView");
+
+            foreach (IWebElement item in options)
+            {
+                item.Click();
+            }
+
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void MedPlusAppTest()
+        {
+            Console.WriteLine("Native App Automation");
+
+            AppiumOptions option = new AppiumOptions();
+            option.AddAdditionalCapability("platformName", "android");
+            option.AddAdditionalCapability("deviceName", "mototrola one");
+            option.AddAdditionalCapability("appActivity", "com.medplus.mobile.android.MainActivity");
+            option.AddAdditionalCapability("appPackage", "com.medplus.mobile.android");
+            option.AddAdditionalCapability("noReset", true);
+            option.AddAdditionalCapability("unlockType", "password");
+            option.AddAdditionalCapability("unlockKey", "Vishnu@123");
+
+            AndroidDriver<IWebElement> driver = new AndroidDriver<IWebElement>(new Uri("http://127.0.0.1:4723/wd/hub"), option);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            ScrollToElementByPercentageAndClick(driver, "Flexi Rewards");
+
+            //Filter
+            driver.FindElementByXPath("//android.widget.Button[contains(@text,'Sort & Filter')]").Click();
+
+            TouchAction action = new TouchAction(driver);
+            action.Tap(230, 970).Perform();
+
+            action.Tap(644, 563).Perform();
+
+            //Add To Cart
+            driver.FindElementByXPath("//android.widget.Button[contains(@text,'Add to Cart')]").Click();
+
+
+            driver.FindElementByAndroidUIAutomator("UiSelector().textContains(\"Mobile Number\")").Click();
+            driver.FindElementByAndroidUIAutomator("UiSelector().textContains(\"Mobile Number\")").SendKeys("8687686800");
+
+            //driver.FindElementByXPath("//android.widget.EditText[contains(@text,'Mobile Number')]").Click();
+            //driver.FindElementByXPath("//android.widget.EditText[contains(@text,'Mobile Number')]").SendKeys("8687686800");
 
             driver.Quit();
         }
@@ -235,7 +409,45 @@ namespace MobAutomation
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             driver.FindElementByAndroidUIAutomator($"UiSelector().textContains(\"{fieldName}\")").Click();
         }
+        public void ScrollToElementByPercentageAndClick(AndroidDriver<IWebElement> driver, string fieldName)
+        {
+            Size size = driver.Manage().Window.Size;
+            Console.WriteLine("Width : " + size.Width);
+            Console.WriteLine("Height : " + size.Height);
 
+            double startX = size.Width / 2.0;
+            double startY = size.Height * 0.80;
+            double endX = startX;
+            double endY = size.Height * 0.20;
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(0);
+            while (driver.FindElementsByAndroidUIAutomator($"UiSelector().textContains(\"{fieldName}\")").Count == 0)
+            {
+                TouchAction action = new TouchAction(driver);
+                action.Press(startX, startY).Wait(1000).MoveTo(endX, endY).Release().Perform();
+            }
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.FindElementByAndroidUIAutomator($"UiSelector().textContains(\"{fieldName}\")").Click();
+        }
+        public void ScrollToElementByUISelectorAndClick(AndroidDriver<IWebElement> driver, string fieldName)
+        {
+            driver.FindElementByAndroidUIAutomator(
+                                        "new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textContains(\"" + fieldName + "\").instance(0))").Click();
+
+            //driver.FindElementByAndroidUIAutomator(
+            //                           "new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textContains(\"" + fieldName + "\").instance(0))");
+        }
+
+        public void ScrollToElementByMobileCommandsAndClick(AndroidDriver<IWebElement> driver, string fieldName)
+        {
+            Dictionary<string, object> dic = new Dictionary<string, object>();
+            dic.Add("strategy", "-android uiautomator");
+            dic.Add("selector", $"UiSelector().textContains(\"{fieldName}\")");
+            driver.ExecuteScript("mobile: scroll", dic);
+
+            driver.FindElementByAndroidUIAutomator($"UiSelector().textContains(\"{fieldName}\")").Click();
+        }
         #endregion
     }
 }
